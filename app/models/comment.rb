bde6c8b39258ab2435ba5a66582c403a2a83cdf8
abrormukimov <abrormm34@gmail.com>
2020-06-17 12:12:36 +0500

    Add title to posts table and presence to comment_field

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  validates :comment_field, length: { maximum: 150 }
+  validates :comment_field, presence: true, length: { maximum: 150 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_122344) do
+ActiveRecord::Schema.define(version: 2020_06_17_065826) do
 
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_06_16_122344) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
