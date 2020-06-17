b6e66588bbf1c19a66146d4b1d2cf9696d2847e0
abrormukimov <abrormm34@gmail.com>
2020-06-17 18:28:08 +0500

    Add validation to title column

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   validates :text_field, presence: true, length: { minimum: 5, maximum: 160 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
 end
